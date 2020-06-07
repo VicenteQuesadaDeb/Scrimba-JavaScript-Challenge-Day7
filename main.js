9a2f0a1e5b0f236aae12d771c2ca2557f616482d
Vicente Quesada Camón <vicentequesadadeb@gmail.com>
2020-06-07 13:54:01 +0200

    main.js solved

--- main.js
+++ main.js
@@ -1,38 +1,47 @@
 function properNounCorrection(str) {
-    //  write code here.
+  //  write code here.
+  //split the word in an array (it gets separated by letter with comas in between)
+  const arrayStr = str.split("");
+  const result = [];
+  //Convert to uppercase first letter
+  //Conver to lowercase the rest of the letters
+  result.push(arrayStr[0].toUpperCase());
+  for (let i = 1; i < arrayStr.length; i++) {
+    result.push(arrayStr[i].toLowerCase());
+  }
+  //Remove the comas and return
+  return result.join("");
 }
 
-
-
 /**
-* Test Suite 
-*/
-describe('properNounCorrection()', () => {
-    it('adds proper noun capitalization', () => {
-        // arrange
-        const str = 'pARiS';
-        
-        // act
-        const result = properNounCorrection(str);
+ * Test Suite
+ */
+describe("properNounCorrection()", () => {
+  it("adds proper noun capitalization", () => {
+    // arrange
+    const str = "pARiS";
 
-        // log
-        console.log("result 1: ", result);
-        
-        // assert
-        expect(result).toBe('Paris');
-    });
-    
-     it('properly cased words are still correct', () => {
-        // arrange
-        const str = 'John';
-        
-        // act
-        const result = properNounCorrection(str);
+    // act
+    const result = properNounCorrection(str);
 
-        // log
-        console.log("result 2: ", result);
-        
-        // assert
-        expect(result).toBe('John');
-    });
+    // log
+    console.log("result 1: ", result);
+
+    // assert
+    expect(result).toBe("Paris");
+  });
+
+  it("properly cased words are still correct", () => {
+    // arrange
+    const str = "John";
+
+    // act
+    const result = properNounCorrection(str);
+
+    // log
+    console.log("result 2: ", result);
+
+    // assert
+    expect(result).toBe("John");
+  });
 });
